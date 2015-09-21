460a01b0906bbd1d788005bfbeaed33e1950cf87
Matthew Yang <yang70@gmail.com>
2015-09-21 16:48:23 -0700

    add generated test info, commented out mostly

---
+++ test/models/league_test.rb
@@ -0,0 +1,11 @@
+# require "test_helper"
+
+# class LeagueTest < ActiveSupport::TestCase
+#   def league
+#     @league ||= League.new
+#   end
+
+#   def test_valid
+#     assert league.valid?
+#   end
+# end
