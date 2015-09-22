51fc0a7689b45f7b103e4f8f772001c48af7adb0
Matthew Yang <yang70@gmail.com>
2015-09-21 17:23:53 -0700

    modify seed to create a league, 20 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 league = League.create( name: "Super Rad Soccer League")
+
+users = []
+
+20.times do
+  name = Faker::Name.name
+  email = Faker::Internet.email(name)
+  user = User.create( name: name, email: email, password: "password", password_confirmation: "password" )
+  users << user
+end
+
+# # seed db with teams and managers, users must be created first
+# teams = []
+
+# (0..9).each do |num|
+#   name = Faker::Team.creature
+#   manager = User.find(num)
+#   team = Team.create( name: name, manager: manager )
+# end
