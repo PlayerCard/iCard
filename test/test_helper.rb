9851ba9e05fe99680a105768807567bfb4a866fe
Ronald Kinch <ronaldkinch@comcast.net>
2015-09-22 12:26:07 -0700

    Add SimpleCov, RuboCop and sign_in(role) to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 ENV["RAILS_ENV"] = "test"
+Rails.env = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
@@ -15,4 +16,14 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
+  def sign_in(role = :player)
+    # Sign in authenticated user
+    visit new_user_session_path
+    fill_in "Email", with: users(role).email
+    fill_in "Name", with: users(role).name
+    fill_in "Password", with: "password"
+    click_on "Log in"
+  end
 end
+
+system 'rubocop'
