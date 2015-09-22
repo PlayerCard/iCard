392504c69ff70cf094fc1899c2c381f41e0c6c73
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-21 17:14:15 -0700

    Add Profile model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921232948) do
+ActiveRecord::Schema.define(version: 20150921234523) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "picture_url"
+    t.string   "role"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "profiles", ["role"], name: "index_profiles_on_role"
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
