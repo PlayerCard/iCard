55647a2e5d8c8bc7b1b70eb618f34e1001bed92d
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-23 14:02:45 -0700

    Add pull_line_up helper to Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,10 @@ class Game < ActiveRecord::Base
   has_many :game_players
   has_and_belongs_to_many :teams
   belongs_to :referee, class_name: 'User'
+
+  def pull_line_up(team)
+    team.line_up.each do |player|
+      game_player.create(game: @game, player: player, team: team)
+    end
+  end
 end
