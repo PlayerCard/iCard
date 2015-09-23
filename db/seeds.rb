f7230ff52b9614ee8556537c63852ccc1dcd1bbe
Ronald Kinch <ronaldkinch@comcast.net>
2015-09-22 21:34:02 -0700

    Sign Up/In thru Profile views. Move name & contact info from User to Profile. Modify seed to match. Add user_root_path & views.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,9 @@ require 'faker'
 league = League.create( name: "Super Rad Soccer League")
 
 # Create player for testing
-user  = User.create!( name: "Test Player",
-                      email: "test@example.com",
+user  = User.create!( email: "test@example.com",
                    password: "password",
-      password_confirmation: "password",
-                  address_1: Faker::Address.street_address,
-                  address_2: Faker::Address.secondary_address,
-                       city: Faker::Address.city,
-                      state: Faker::Address.state_abbr,
-                        zip: Faker::Address.zip )
+      password_confirmation: "password" )
 
 # Create players
 players = []
@@ -19,20 +13,18 @@ players = []
 200.times do
   name = Faker::Name.name
   email = Faker::Internet.email(name)
-  user = User.create!(
-                      email: email,
-                   password: "password",
-      password_confirmation: "password"
-                         )
+  user = User.create!( email: email,
+                    password: "password",
+       password_confirmation: "password" )
   player = Profile.create!( user: user,
-                       name: name,
-                  address_1: Faker::Address.street_address,
-                  address_2: Faker::Address.secondary_address,
-                       city: Faker::Address.city,
-                      state: Faker::Address.state_abbr,
-                        zip: Faker::Address.zip,
-                picture_url: Faker::Avatar.image,
-                       role: "player" )
+                        name: name,
+                   address_1: Faker::Address.street_address,
+                   address_2: Faker::Address.secondary_address,
+                        city: Faker::Address.city,
+                       state: Faker::Address.state_abbr,
+                         zip: Faker::Address.zip,
+                 picture_url: Faker::Avatar.image,
+                        role: "player" )
   players << player
 end
 
@@ -42,20 +34,18 @@ refs = []
 5.times do
   name = Faker::Name.name
   email = Faker::Internet.email(name)
-  user = User.create!(
-                      email: email,
-                   password: "password",
-      password_confirmation: "password"
-                         )
+  user = User.create!( email: email,
+                    password: "password",
+       password_confirmation: "password" )
   ref = Profile.create!( user: user,
-                       name: name,
-                  address_1: Faker::Address.street_address,
-                  address_2: Faker::Address.secondary_address,
-                       city: Faker::Address.city,
-                      state: Faker::Address.state_abbr,
-                        zip: Faker::Address.zip,
-                picture_url: Faker::Avatar.image,
-                       role: "referee" )
+                        name: name,
+                   address_1: Faker::Address.street_address,
+                   address_2: Faker::Address.secondary_address,
+                        city: Faker::Address.city,
+                       state: Faker::Address.state_abbr,
+                         zip: Faker::Address.zip,
+                 picture_url: Faker::Avatar.image,
+                        role: "referee" )
   refs << ref
 end
 
