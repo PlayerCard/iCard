250c2d1f842127887eb0cff2a25a4a71f2a7f894
Ronald Kinch <ronaldkinch@comcast.net>
2015-09-23 18:44:16 -0700

    Making sure both files are pushed

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -65,7 +65,7 @@ class LeaguesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_league
-      @league = League.find(1)
+      @league = League.first
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
