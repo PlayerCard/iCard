283e9ab5494ad57b36f9388dc1b35af11f63a8c7
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-24 16:55:58 -0700

    Pass create team test
    Add 'name' field to new team form
    Update strong params in team controller
    Revise test to match flash

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -71,6 +71,6 @@ class TeamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def team_params
-      params[:team]
+      params.require(:team).permit(:name)
     end
 end
