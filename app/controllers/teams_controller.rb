3732fb45b731217528c72c34b5fab8c7045f5d71
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-25 10:46:09 -0700

    Move lineup size check to private method

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -28,18 +28,11 @@ class TeamsController < ApplicationController
   end
 
   def update
-    in_lineup = params[:team][:team_memberships_attributes]
-    lineup_count = 0
-    in_lineup.each { |player| lineup_count += 1 if player[1][:in_line_up] == '1' }
-    if lineup_count > 20
-      flash.now[:alert] = "Cannot select more than 20 players for lineup, you selected #{lineup_count}."
-      render :edit
+    check_line_up_size(params)
+    if @team.update(team_params)
+      redirect_to @team, notice: 'Team was successfully updated.'
     else
-      if @team.update(team_params)
-        redirect_to @team, notice: 'Team was successfully updated.'
-      else
-        render :edit
-      end
+      render :edit
     end
   end
 
@@ -49,6 +42,16 @@ class TeamsController < ApplicationController
   end
 
   private
+
+  def check_line_up_size(params)
+    in_lineup = params[:team][:team_memberships_attributes]
+    lineup_count = 0
+    in_lineup.each { |player| lineup_count += 1 if player[1][:in_line_up] == '1' }
+    if lineup_count > 20
+      flash.now[:alert] = "Cannot select more than 20 players for lineup, you selected #{lineup_count}."
+      render :edit
+    end
+  end
 
   def set_team
     @team = Team.find(params[:id])
