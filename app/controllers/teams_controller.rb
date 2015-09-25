d178f1a845e777cc077d625d82bdc0a6ea4cb00c
Matthew Yang <yang70@gmail.com>
2015-09-24 17:31:17 -0700

    lineup check boxes appearing on team edit page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -68,6 +68,6 @@ class TeamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def team_params
-      params.require(:team).permit(:name)
+      params.require(:team).permit(:name, team_memberships: [:id, :player_id, :in_line_up, :is_manager])
     end
 end
