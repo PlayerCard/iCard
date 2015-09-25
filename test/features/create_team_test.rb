089e7fe35a750aef7a85d5f8d77d2000c8634961
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-24 17:01:00 -0700

    Add update team test (passing)

--- test/features/create_team_test.rb
+++ test/features/create_team_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-feature "Create team" do
+feature "Teams have crud actions" do
   scenario "create a new team" do
     # Given a signed-id user at a new team page
     sign_in
@@ -13,5 +13,19 @@ feature "Create team" do
     # Then a team is created
     page.must_have_content "Team was successfully created."
     page.must_have_content "Seattle Sounders FC"
+  end
+
+  scenario "update a team" do
+    # Given a signed-in user and an existing team
+    sign_in
+    visit teams_path
+    click_on "Tigers"
+    click_on "Edit"
+    # When I update a field and submit
+    fill_in "Name", with: "Leopards"
+    click_on "Update Team"
+    # Then the team is updated
+    page.must_have_content "Team was successfully updated."
+    page.must_have_content "Leopards"
   end
 end
