335a2bea4cd3c6b688b102e31fa13e1b0717908a
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-25 13:58:07 -0700

    Test update profile unhappy path (passing)

--- test/features/user_profile/edit_profile_test.rb
+++ test/features/user_profile/edit_profile_test.rb
@@ -10,4 +10,16 @@ feature "Edit A Profile" do
     page.must_have_content "Profile was successfully updated"
     page.must_have_content "Stefan Frei"
   end
+
+  scenario "wont update invalid profile" do
+    # Given an existing profile page
+    sign_in(:player_1)
+    visit edit_profile_path(profiles(:player_1_profile))
+    # When I submit invalid data
+    fill_in "Name", with: ""
+    click_on "Update Profile"
+    # The profile is not updated
+    page.must_have_content "prohibited this profile from being saved:"
+    page.must_have_content "Name can't be blank"
+  end
 end
